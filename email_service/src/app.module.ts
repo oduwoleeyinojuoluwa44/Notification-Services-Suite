d0a7fbb00fc843579bb09f06ebb210c3c24dd156
chiefEbube <ebubechi.anyanwu@yahoo.com>
2025-11-12 14:53:22 +0100

    refactor(email_service): Remove caching logic and related imports from AppController and AppModule
    - Eliminated CacheManager injection and associated caching logic from AppController's health check.
    - Removed CacheModule registration from AppModule to streamline dependencies and reduce complexity.

--- email_service/src/app.module.ts
+++ email_service/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { SendgridModule } from './sendgrid/sendgrid.module';
 import { EmailModule } from './email/email.module';
-import { CacheModule } from '@nestjs/cache-manager';
-import { redisStore } from 'cache-manager-redis-store';
 import { TerminusModule } from '@nestjs/terminus';
 
 
@@ -15,16 +13,6 @@ import { TerminusModule } from '@nestjs/terminus';
       isGlobal: true,
     }),
     TerminusModule,
-    CacheModule.registerAsync({
-      isGlobal: true,
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        store: redisStore,
-        url: configService.getOrThrow<string>('REDIS_URL'),
-        ttl: 3600,
-      }),
-    }),
     SendgridModule,
     EmailModule,
   ],
