1fa6a8a3f931996ccbc1da779cdb8876f2da4c6f
chiefEbube <ebubechi.anyanwu@yahoo.com>
2025-11-13 20:31:27 +0100

    fix

--- user_service/app/main.py
+++ user_service/app/main.py
@@ -30,7 +30,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8000", "http://127.0.0.1:8000"],
+    allow_origins=["*"],  # Allow all origins for production
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
