6feaf1ecc6508178dce9c77c7b59f1ebdf456a3b
chiefEbube <ebubechi.anyanwu@yahoo.com>
2025-11-13 17:07:18 +0100

    chore: Remove commented-out route registration in index.js
    - Deleted commented-out code for user routes in index.js to clean up the file and improve readability.

--- api_gateway/src/routes/index.js
+++ api_gateway/src/routes/index.js
@@ -3,8 +3,6 @@ const fp = require('fastify-plugin');
 
 async function apiRoutes(fastify, options) {
     fastify.register(require('./notification.routes'), { prefix: '/api/v1/notifications' });
-    // Add other route registrations here if needed, e.g.:
-    // fastify.register(require('./user.routes'), { prefix: '/api/v1/users' });
 }
 
 module.exports = fp(apiRoutes);
